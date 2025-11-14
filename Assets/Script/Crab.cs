0b898b046f18c28215e901b0a4cf5d2168509944
Lililica <lucas.terrasson@gmail.com>
2025-11-14 11:10:19 +0100

    init crab stats

--- Assets/Script/Crab.cs
+++ Assets/Script/Crab.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class Crab : MonoBehaviour
 {
+
+    // Crab stats
+    [Header ("Crab Stats")]
+
+    [SerializeField] 
+    private float m_speed = 5f;
+
+    [SerializeField]
+    private int m_weight = 10;
+
+    private Vector3 m_direction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
